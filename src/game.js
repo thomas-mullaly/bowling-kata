ddbf45a766cd7d77f022a7562a9cab29dfa0917d
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-13 14:23:04 -0400

    Refactored game logic

--- src/game.js
+++ src/game.js
@@ -14,23 +14,39 @@ class Game {
         let frameIndex = 0;
 
         for (let frame = 0; frame < 10; ++frame) {
-            let frameTotal = 0;
-
-            if (this._rolls[frameIndex] === 10) {
-                frameTotal += 10 + this._rolls[frameIndex+1] + this._rolls[frameIndex+2];
+            if (this._isStrike(frameIndex)) {
+                score += 10 + this._strikeBonus(frameIndex);
                 frameIndex += 1;
-            } else if (this._rolls[frameIndex] + this._rolls[frameIndex+1] === 10) {
-                frameTotal += 10 + this._rolls[frameIndex+1];
+            } else if (this._isSpare(frameIndex)) {
+                score += 10 + this._spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                frameTotal += this._rolls[frameIndex] + this._rolls[frameIndex+1];
+                score += this._frameTotal(frameIndex);
                 frameIndex += 2;
             }
-
-            score += frameTotal;
         }
 
         return score;
+    }
+
+    _isSpare(frameIndex) {
+        return this._rolls[frameIndex] + this._rolls[frameIndex+1] === 10;
+    }
+
+    _isStrike(frameIndex) {
+        return this._rolls[frameIndex] === 10;
+    }
+
+    _spareBonus(frameIndex) {
+        return this._rolls[frameIndex+1];
+    }
+
+    _strikeBonus(frameIndex) {
+        return this._rolls[frameIndex+1] + this._rolls[frameIndex+2];
+    }
+
+    _frameTotal(frameIndex) {
+        return this._rolls[frameIndex] + this._rolls[frameIndex+1];
     }
 }
 
