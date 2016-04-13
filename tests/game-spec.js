bf10faf9f227917898e79ffa14c3a8c0b83e154a
Thomas Mullaly <thomas.mullaly@gmail.com>
2016-04-13 12:48:01 -0400

    Added test for spare condition

--- tests/game-spec.js
+++ tests/game-spec.js
@@ -4,28 +4,41 @@ let should = require("should");
 let Game = require("../src/game");
 
 describe("Game", function() {
-    describe("roll", function() {
-        const maxRolls = 20;
-        let game = null;
+    const maxRolls = 20;
+    let game = null;
 
-        beforeEach(function () {
-            game = new Game();
-        });
+    beforeEach(function () {
+        game = new Game();
+    });
 
-        it("should return 0 for gutter game", function() {
-            for (let i = 0; i < maxRolls; ++i) {
-                game.roll(0);
-            }
+    let rollMany = (rolls, pins) => {
+        for (let i = 0; i < rolls; ++i) {
+            game.roll(pins);
+        }
+    };
 
-            game.score().should.equal(0);
-        });
+    it("should return 0 for gutter game", function() {
+        for (let i = 0; i < maxRolls; ++i) {
+            game.roll(0);
+        }
 
-        it("should return 20 for all ones", function () {
-            for (let i = 0; i < maxRolls; ++i) {
-                game.roll(1);
-            }
+        game.score().should.equal(0);
+    });
 
-            game.score().should.equal(20);
-        });
+    it("should return 20 for all ones", function () {
+        for (let i = 0; i < maxRolls; ++i) {
+            game.roll(1);
+        }
+
+        game.score().should.equal(20);
+    });
+
+    it("should correctly handle 1 spare", function () {
+        game.roll(4);
+        game.roll(6); // score: 16
+        game.roll(6); // score: 22
+        rollMany(17, 0);
+
+        game.score().should.equal(22);
     });
 });
